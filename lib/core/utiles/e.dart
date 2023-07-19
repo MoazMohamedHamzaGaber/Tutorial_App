e2f94e021432247805ed1273752c563d5ed865d5
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2023-07-20 02:09:14 +0300

    home from moaz

--- lib/core/utiles/e.dart
+++ lib/core/utiles/e.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class s extends StatelessWidget {
-  const s({Key? key}) : super(key: key);
+class Ex extends StatelessWidget {
+  const Ex({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
